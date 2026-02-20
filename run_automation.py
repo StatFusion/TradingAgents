fdd29c0383087776f0b9ca762c396e359de4152a
StatFusion <122210428+StatFusion@users.noreply.github.com>
2026-02-21 03:57:32 +0800

    Update run_automation.py

--- run_automation.py
+++ run_automation.py
@@ -64,7 +64,7 @@ reports_dir = "reports"
 if not os.path.exists(reports_dir):
     os.makedirs(reports_dir)
 
-stock_list = ["AMZN","ASML","XOM","CVX","MA","V","KO","CRCL","VTI","VEEV","TSM","NOW"]
+stock_list = ["AMZN","MA","V","VTI","TSM","NOW","COST","VST","RTX"]
 
 # 增加一个线程锁，专门用来防止初始化时 API Key 被覆盖
 init_lock = threading.Lock()
